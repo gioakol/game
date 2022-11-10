6dc7e8711b336e4d2ee3c668cc13b32226cd68aa
Giovanni Beltran <gioakol@gmail.com>
2022-11-10 17:48:57 -0500

    Se protege archivos iniciales

---
+++ src/game/controller/FXMLUIController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Janus
+ */
+public class FXMLUIController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private Button button;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        label.setText("he cambiado el texto");
+        button.setText("ok");
+    }    
+    
+}
